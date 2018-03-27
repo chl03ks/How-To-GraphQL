a8520bbdd568cffe19ed18eb0b849aa7289d7a94
Daniel <danielulisesverdin@gmail.com>
2018-03-27 11:34:53 -0600

    createVideo method

--- src/data/index.js
+++ src/data/index.js
@@ -21,7 +21,20 @@ const getVideoById = (id) => new Promise(resolve => {
 
 const getVideos = () => new Promise(resolve => resolve(videos));
 
+const createVideo = ({ title, duration , watched}) => {
+  const video = {
+    id: (new Buffer(title, 'utf8').toString('base64')),
+    title,
+    duration,
+    watched
+  };
+  videos.push(video);
+
+  return video;
+};
+
 module.exports = {
   getVideoById,
-  getVideos
+  getVideos,
+  createVideo
 }
